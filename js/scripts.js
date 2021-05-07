f6168bd9abe93b3f09a95140e42af08f4f377eb4
Lucian <lucian.miller185@gmail.com>
2021-05-07 11:18:23 -0700

    It will delete console log and unnecessary spaces

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,10 @@ $(document).ready(function() {
     const favoriteThing1 = $("#input1").val();
     const favoriteThing2 = $("#input2").val();
     const favoriteThing3 = $("#input3").val();
-
     let array = []
     array.push(favoriteThing1);
     array.push(favoriteThing2);
     array.push(favoriteThing3);
-    console.log(array);
     $("ul").prepend("<li>" + array[2] + "</li>");
     $("ul").prepend("<li>" + array[1] + "</li>");
     $("ul").prepend("<li>" + array[0] + "</li>");
