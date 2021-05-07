a14b85f40c000e9a3c30c4c5dec276afbcc47d80
Lucian <lucian.miller185@gmail.com>
2021-05-07 10:58:53 -0700

    It will take information from the form and add it to an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
   $("form#form").submit(function(event) {
     event.preventDefault();
+    const favoriteThing = $("#input1").val();
+
+    let array = []
+    array.push(favoriteThing);
+    console.log(array);
+    $("ul").prepend("<li>" + array[0] + "</li>");
   });
 });
